2da7c3fa82cc93768037739683a5df6ce76cf521
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-07-24 22:53:57 +0900

    chore: update nuxt config and apply bootstrap-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,62 @@
-const parseArgs = require("minimist")
+const parseArgs = require('minimist')
 const argv = parseArgs(process.argv.slice(2), {
   alias: {
-    H: "hostname",
-    p: "port"
+    H: 'hostname',
+    p: 'port',
   },
-  string: ["H"],
-  unknown: parameter => false
+  string: ['H'],
+  unknown: parameter => false,
 })
 
 const port =
   argv.port ||
   process.env.PORT ||
   process.env.npm_package_config_nuxt_port ||
-  "3000"
+  '3000'
 const host =
   argv.hostname ||
   process.env.HOST ||
   process.env.npm_package_config_nuxt_host ||
-  "localhost"
+  'localhost'
 module.exports = {
   env: {
-    baseUrl:
-      process.env.BASE_URL ||
-      `http://${host}:${port}`
+    baseUrl: process.env.BASE_URL || `http://${host}:${port}`,
   },
   head: {
-    title: "tt1",
+    title: 'Cactus',
     meta: [
-      { charset: "utf-8" },
+      { charset: 'utf-8' },
       {
-        name: "viewport",
-        content:
-          "width=device-width, initial-scale=1"
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
       },
       {
-        hid: "description",
-        name: "description",
-        content: "Nuxt.js project"
-      }
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project',
+      },
     ],
     link: [
       {
-        rel: "icon",
-        type: "image/x-icon",
-        href: "/favicon.ico"
-      }
-    ]
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+    ],
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: "#3B8070" },
+  loading: { color: '#3B8070' },
   /*
   ** Build configuration
   */
-  css: ["~/assets/css/main.css"],
+  css: ['@/assets/css/app.sass'],
   build: {},
   modules: [
-    "@nuxtjs/axios",
-    "~/modules/typescript.js"
+    ['bootstrap-vue/nuxt', { css: false }],
+    '@nuxtjs/axios',
+    '~/modules/typescript.js',
   ],
-  axios: {}
+  axios: {},
 }
