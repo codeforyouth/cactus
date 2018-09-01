60e236b6a75f02d010f5d3a6b1edc01ca76fdabc
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-09-01 14:59:28 +0900

    feat(Store): move projects data to vuex store from inline

--- pages/index.vue
+++ pages/index.vue
@@ -28,28 +28,8 @@ export default class extends Vue {
   heroTitle: string = 'Cactus Project'
   heroDescription: string =
     '地域の課題に取り組むプロジェクトと\n様々な興味を持った若者をつなぐための\nプラットフォーム「Cactas」を運営しています。\n\n下に並んでいる応募可能なプロジェクト一覧の中から\nあなたが参加したい・話が聞きたいプロジェクトを探して\n詳細ページから応募することができます。'
-  projects: Array<object> = [
-    {
-      id: 1,
-      title:
-        '給食の献立をオンライン化！\n4919プロジェクトを一緒に全国展開するエンジニアの方を探しています',
-      image: 'image/project_sample.jpg',
-      auther: '河中祥吾',
-      tags: ['食', '奈良', 'エンジニア'],
-      endsOn: '6/30',
-    },
-    {
-      id: 2,
-      title:
-        '給食の献立をオンライン化！\n4919プロジェクトを一緒に全国展開するエンジニアの方を探しています',
-      image: 'image/project_sample.jpg',
-      auther: '河中祥吾',
-      tags: ['食', '奈良', 'エンジニア'],
-      endsOn: '6/30',
-    },
-  ]
-  // @State
-  // projects
+  @State
+  projects
 }
 </script>
 <style scoped>
